d6524f69820ea53cb70a7756563909dc1da5e97f
khanghtse <khanghoang090703@gmail.com>
2025-11-27 10:52:47 +0700

    ai controller, routers for function enhance job desc and professional summary

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import "dotenv/config";
 import connectDB from './src/configs/db.js';
 import userRouter from './src/routes/userRoutes.js';
 import resumeRouter from './src/routes/resumeRoutes.js';
+import aiRouter from './src/routes/aiRoutes.js';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -17,6 +18,7 @@ app.use(express.json());
 app.get('/', (req, res) => res.send('Hello World!'));
 app.use('/api/users', userRouter);
 app.use('/api/resumes', resumeRouter);
+app.use('/api/ai', aiRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
